122e44341a1c502bbc16648f45658b00cb07d111
Yunus Hossain <yonusprince@gmail.com>
2024-08-16 10:48:40 -0400

    largely finished the player class and laser class

---
+++ code/laser_class.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, player_x_position : int) -> None:
+        super().__init__()
+        self.image = pygame.image.load("assets//laser.png").convert_alpha()
+        self.rect = self.image.get_rect(midbottom=(player_x_position, 75))
+    def destroy(self) -> None:
+        if self.rect.bottom <= 0:
+            self.kill()
+    def update(self) -> None:
+        self.destroy()
+        self.rect.y -= 5
--- code/player_class.py
+++ code/player_class.py
@@ -1,16 +1,18 @@
 import pygame
 from event_info import EventInfo
+from laser_class import Laser
 
-class Player(pygame.sprite.Sprite):
+class Player():
     def __init__(self) -> None:
-        super().__init__()
-        self.player_fly_0 = pygame.image.load("assets//player_spaceship//player_spaceship_fly_0.png")
-        self.player_fly_1 = pygame.image.load("assets//player_spaceship//player_spaceship_fly_1.png")
+        self.player_fly_0 = pygame.image.load("assets//player_spaceship//player_spaceship_fly_0.png").convert_alpha()
+        self.player_fly_1 = pygame.image.load("assets//player_spaceship//player_spaceship_fly_1.png").convert_alpha()
         self.player_frames = [self.player_fly_0, self.player_fly_1]
         self.animation_index = 0
 
         self.image = self.player_frames[self.animation_index]
         self.rect = self.image.get_rect(midbottom=(400, 450))
+
+        self.lasers = pygame.sprite.Group()
     
     def animate(self) -> None:
         self.animation_index += 0.1
@@ -25,3 +27,20 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= 5
         elif keys_pressed[pygame.K_RIGHT]:
             self.rect.x += 5
+    
+    def shoot_laser(self, event_info : EventInfo):
+        events = event_info["events"]
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.lasers.add(Laser(self.rect.centerx))
+    
+    def update(self, event_info : EventInfo):
+        self.animate()
+        self.movement(event_info)
+        self.shoot_laser(event_info)
+    
+    def draw(self, screen : pygame.Surface):
+        screen.blit(self.image, self.rect)
+        self.lasers.update()
+        self.lasers.draw()
